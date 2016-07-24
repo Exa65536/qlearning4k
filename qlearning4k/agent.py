3adafa953c96acbd7e94baa39978e663b46f1f7e
Exa65536 <quantum935@gmail.com>
2016-07-24 09:35:10 -0500

    Add winrate readout, pygame visualization

--- qlearning4k/agent.py
+++ qlearning4k/agent.py
@@ -56,79 +56,94 @@ class Agent:
 	def clear_frames(self):
 		self.frames = None
 
-	def train(self, game, nb_epoch=1000, batch_size=50, gamma=0.9, epsilon=[1., .1], epsilon_rate=0.5, reset_memory=False):
-		self.check_game_compatibility(game)
-		if type(epsilon)  in {tuple, list}:
-			delta =  ((epsilon[0] - epsilon[1]) / (nb_epoch * epsilon_rate))
-			final_epsilon = epsilon[1]
-			epsilon = epsilon[0]
-		else:
-			final_epsilon = epsilon
-		model = self.model
-		nb_actions = model.output_shape[-1]
-		win_count = 0
-		for epoch in range(nb_epoch):
-			loss = 0.
-			game.reset()
-			self.clear_frames()
-			if reset_memory:
-				self.reset_memory()
-			game_over = False
-			S = self.get_game_data(game)
-			while not game_over:
-				if np.random.random() < epsilon:
-					a = int(np.random.randint(game.nb_actions))
-				else:
-					q = model.predict(S)
-					a = int(np.argmax(q[0]))
-				game.play(a)
-				r = game.get_score()
-				S_prime = self.get_game_data(game)
-				game_over = game.is_over()
-				transition = [S, a, r, S_prime, game_over]
-				self.memory.remember(*transition)
-				S = S_prime
-				batch = self.memory.get_batch(model=model, batch_size=batch_size, gamma=gamma)
-				if batch:
-					inputs, targets = batch
-					loss += float(model.train_on_batch(inputs, targets))
-			if game.is_won():
-				win_count += 1
-			if epsilon > final_epsilon:
-				epsilon -= delta
-			print("Epoch {:03d}/{:03d} | Loss {:.4f} | Epsilon {:.2f} | Win count {}".format(epoch + 1, nb_epoch, loss, epsilon, win_count))
+	def train(self, game, nb_epoch=1000, batch_size=50, gamma=0.9, epsilon=[1., .1], epsilon_rate=0.5,
+              reset_memory=False):
+        self.check_game_compatibility(game)
+        if type(epsilon) in {tuple, list}:
+            delta = ((epsilon[0] - epsilon[1]) / (nb_epoch * epsilon_rate))
+            final_epsilon = epsilon[1]
+            epsilon = epsilon[0]
+        else:
+            final_epsilon = epsilon
+        model = self.model
+        nb_actions = model.output_shape[-1]
+        win_count = 0
+        for epoch in range(nb_epoch):
+            loss = 0.
+            game.reset()
+            self.clear_frames()
+            if reset_memory:
+                self.reset_memory()
+            game_over = False
+            S = self.get_game_data(game)
+            while not game_over:
+                if np.random.random() < epsilon:
+                    a = int(np.random.randint(game.nb_actions))
+                else:
+                    q = model.predict(S)
+                    a = int(np.argmax(q[0]))
+                game.play(a)
+                r = game.get_score()
+                S_prime = self.get_game_data(game)
+                game_over = game.is_over()
+                transition = [S, a, r, S_prime, game_over]
+                self.memory.remember(*transition)
+                S = S_prime
+                batch = self.memory.get_batch(model=model, batch_size=batch_size, gamma=gamma)
+                if batch:
+                    inputs, targets = batch
+                    loss += float(model.train_on_batch(inputs, targets))
+            if game.is_won():
+                win_count += 1
+            if epsilon > final_epsilon:
+                epsilon -= delta
+            print("Epoch {:03d}/{:03d} | Loss {:.4f} | Epsilon {:.2f} | Win count {} | Win rate {:.3f}".format(epoch + 1, nb_epoch, loss,
+                                                                                             epsilon, win_count, win_count/(epoch+1)))
 
-	def play(self, game, nb_epoch=10, epsilon=0., visualize=True):
-		self.check_game_compatibility(game)
-		model = self.model
-		win_count = 0
-		frames = []
-		for epoch in range(nb_epoch):
-			game.reset()
-			self.clear_frames()
-			S = self.get_game_data(game)
-			if visualize:
-				frames.append(game.draw())
-			game_over = False
-			while not game_over:
-				if np.random.rand() < epsilon:
-					print("random")
-					action = int(np.random.randint(0, game.nb_actions))
-				else:
-					q = model.predict(S)			
-					action = int(np.argmax(q[0]))
-				game.play(action)
-				S = self.get_game_data(game)
-				if visualize:
-					frames.append(game.draw())
-				game_over = game.is_over()
-			if game.is_won():
-				win_count += 1
-		print("Accuracy {} %".format(100. * win_count / nb_epoch))
-		if visualize:
-			if 'images' not in os.listdir('.'):
-				os.mkdir('images')
-			for i in range(len(frames)):
-				plt.imshow(frames[i], interpolation='none')
-				plt.savefig("images/" + game.name + str(i) + ".png")
- 
+    def play(self, game, nb_epoch=10, epsilon=0., visualize=True):
+        self.check_game_compatibility(game)
+        model = self.model
+        win_count = 0
+        frames = []
+        for epoch in range(nb_epoch):
+            game.reset()
+            self.clear_frames()
+            S = self.get_game_data(game)
+            if visualize:
+                frames.append(copy.deepcopy(game.draw()))
+            game_over = False
+            while not game_over:
+                if np.random.rand() < epsilon:
+                    print("random")
+                    action = int(np.random.randint(0, game.nb_actions))
+                else:
+                    q = model.predict(S)
+                    action = int(np.argmax(q[0]))
+                game.play(action)
+                S = self.get_game_data(game)
+                if visualize:
+                    frames.append(copy.deepcopy(game.draw()))
+                game_over = game.is_over()
+            if game.is_won():
+                win_count += 1
+            print("Epoch {:03d}/{:03d}| Epsilon {:.2f} | Win count {} | Win rate {:.3f}".format(int(epoch + 1), int(nb_epoch), epsilon, win_count, win_count/(epoch+1)))
+        print("Accuracy {} %".format(100. * win_count / nb_epoch))
+        if visualize:
+            if 'images' not in os.listdir('.'):
+                os.mkdir('images')
+            pygame.init()
+            print(frames[0].shape)
+            screen = pygame.display.set_mode((frames[0].shape[0]*200,frames[0].shape[1]*200))
+            screen.fill((0, 0, 0))
+            for i in range(len(frames)):
+                plt.imshow(frames[i], interpolation='none')
+                if i == 0:
+                    cbar = plt.colorbar()
+                else:
+                    cbar.set_clim(frames[i].min(), frames[i].max())
+                plt.savefig("images/" + game.name +".png")
+                myimage = pygame.image.load("images/" + game.name +".png")
+                newimage = pygame.transform.scale(myimage, (frames[0].shape[0]*200,frames[0].shape[1]*200))
+                imagerect = newimage.get_rect()
+                screen.blit(newimage, imagerect)
+                pygame.display.flip()
